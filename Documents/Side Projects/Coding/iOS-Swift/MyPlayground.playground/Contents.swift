ba8a92bde27daa77432b8e2280db8b17804beb0e
Anna <iwannabetall@gmail.com>
2017-11-05 15:53:19 -0500

    done

---
+++ Documents/Side Projects/Coding/iOS-Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+var favoriteWord = "mercurial"    // Use your own favorite word!
+var favoriteColor = "blue"        // Use your own favorite color!
+var favoriteMusician = "Nirvana"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+print("I'm printing a string in Swift!")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi mom, my favorite color is \(favoriteColor) \(favoriteWord) \(favoriteMusician) ")
+
+var ninthPlanet: String
+
+let thirdPlanet: String
+
+let fifthPlanet: String = "Jupiter"
+// fifthPlanet is a constant of type ______
+
+// sixthPlanet is a constant of type ______
+
+let seventhPlanet = "Uranus"
+// seventhPlanet is a constant of type ______
+
+let sixthPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+print("\(sixthPlanet) has \(numberOfMoonsOfSaturn) moons.")
+
+func sayHello() {
+    let greeting = "hello there"
+    print(greeting)
+}
+
+var myname = "Anna"
+
+func sayHello(name:String) {
+    print("hello \(name)")
+}
+
+sayHello(name: myname)
+
